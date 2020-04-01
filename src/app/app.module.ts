9f3c04275c9cfc9926205f7aa9aafe47772c5906
Patrik de Oliveira França <patrikoliveira@yahoo.com.br>
2020-04-01 16:59:25 -0300

    componentizando o item da lista de pokemons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './views/list/list.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,

5ec7bda160aa132373129b02516a2a40286b6e27
Patrik de Oliveira França <patrikoliveira@yahoo.com.br>
2020-04-02 10:46:16 -0300

    correção na diretiva que rotaciona os objetos

--- src/app/directives/roll-on-scroll.directive.ts
+++ src/app/directives/roll-on-scroll.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 export class RollOnScrollDirective {
 
   @HostListener('window:scroll',[]) onWindowScroll(){
-    const rotation = 'translateY(-50%) rotateZ(${window.scrollY / 15}deg)';
+    const rotation = `translateY(-50%) rotateZ(${window.scrollY / 15}deg)`;
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       'transform',
